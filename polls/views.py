a66315a88160d7fe67cf58c84b9d0bd2e9be4434
LAPTOP-H86GJAI6\tamtz <tamtzekin@gmail.com>
2020-05-30 22:59:05 +1000

    Votes increment in results.html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
-from django.template import loader
+# from django.http import Http404
+# from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 from .models import Choice, Question
 
 def detail(request, question_id):
