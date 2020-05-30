b591541073a7710d94357b458a76a713f697475f
LAPTOP-H86GJAI6\tamtz <tamtzekin@gmail.com>
2020-05-30 22:51:28 +1000

    Polling app with input fields

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # eg: /polls/5/
+    path('', views.index, name='index'),
+    # eg: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # eg: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # eg: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
