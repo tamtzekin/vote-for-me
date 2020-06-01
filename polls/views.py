b88220f1c222f4c7bde0c0cba44d7cd4c7428406
LAPTOP-H86GJAI6\tamtz <tamtzekin@gmail.com>
2020-06-01 21:24:55 +1000

    Refactored based on generic views

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,25 @@
-from django.http import HttpResponse, HttpResponseRedirect
-# from django.http import Http404
-# from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -34,8 +38,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from beign posted twice if a 
         # user hits the Back button. 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
